68242c8902fcde494ec7df4522ec6e73fcb2249c
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-27 13:01:43 -0400

    new solution using function.

---
+++ solution4-function.py
@@ -0,0 +1,20 @@
+print("Prints a list of integers given two integers as input")
+
+x = int(input("Enter first number: "))
+y = int(input("Enter last number:  "))
+
+def printListOfInts(x, y):
+    i = x
+    if x <= y:
+        step = 1
+        start = x
+        stop = y + 1
+    else:
+        step = -1
+        start = x
+        stop = y - 1
+
+    for i in range(start, stop, step):
+        print(i)
+
+printListOfInts(x,y)
