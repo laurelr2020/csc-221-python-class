45e0908fdfdac70536476e0fe75278ed2ea7b015
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-29 12:37:48 -0400

    created and started assignment 1.

---
+++ Program Assignments/count-occurances.py
@@ -0,0 +1,7 @@
+# Write a Python program named count-occurrences.py that will take one command-line argument 
+# and count the number of times that argument occurs on the standard input, 
+# then print the count to standard output.
+
+import sys
+
+print(len(sys.argv))
