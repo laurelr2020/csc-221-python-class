4dc9050a543069deaef591bd0d97c307fc75616e
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-29 12:57:10 -0400

    read lines and print.

---
+++ find-csc.py
@@ -0,0 +1,7 @@
+#This program finds the first occuracne of the string "CSC" in the input stream and prints the line number it occurs on.
+#The search is case-insenstitive
+import sys
+
+lines = sys.stdin.readlines()
+
+print (lines)
