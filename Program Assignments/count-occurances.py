714ca7322ed0f1efd2834835c95a24b1dfb9bad8
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-31 20:37:43 -0400

    assignment 1 case insensitive

--- Program Assignments/count-occurances.py
+++ Program Assignments/count-occurances.py
@@ -4,4 +4,13 @@
 
 import sys
 
-print(len(sys.argv))
+lines = sys.stdin.readlines()
+
+lineNumber = 0
+count = 0
+for line in lines:
+        lineNumber += 1
+        if(line.lower().find(sys.argv[1]) != -1):
+                count +=1
+
+sys.stdout.write("That occured " + str(count) + " times\n")
