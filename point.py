94cb1bf042e8052afe17b81e4343cfe3543f1353
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-03 13:01:22 -0400

    created method of class.

--- point.py
+++ point.py
@@ -1,3 +1,7 @@
 class Point:
-    """Point is a representation of a two dimensional point as in analytic geometry"""
-    
+    """Point is a representation of a two dimensional point as in analytic geometry. There are two attributes, x and y."""
+
+    def reset(self):
+        self.x = 0
+        self.y = 0
+
