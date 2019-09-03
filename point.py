83878e5e183863dd666a2600b6232d90a07d6228
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-03 13:10:22 -0400

    move method.

--- point.py
+++ point.py
@@ -1,6 +1,10 @@
 class Point:
     """Point is a representation of a two dimensional point as in analytic geometry. There are two attributes, x and y."""
 
+    def move(self, newX, newY):
+        self.x = newX
+        self.y = newY
+        
     def reset(self):
         self.x = 0
         self.y = 0
