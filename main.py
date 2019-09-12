b281a5bc912f6f8897473ee968db4ae8e6d15088
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-12 13:02:47 -0400

    too many params

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from geometry.rectangle import Rectangle
 def main():
     """  This is the starting point"""
     p = Point(3,2)
-    r = Rectangle(p.x, p.y, 100, 200)
+    r = Rectangle(p, 100, 200)
     print("The area is {0}".format(r.calculateArea()))
 
 if __name__ == "__main__":
