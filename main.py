c41b937093f8611907cd091dd01b6d6377ea15eb
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-12 13:22:22 -0400

    added help statment.

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ def main():
     r = Rectangle(p, 100, 200)
     print("The area is {0}".format(r.calculateArea()))
 
+"""This if statement will only run with the main method when this script is given on the python command line"""
+print(__name__)
 if __name__ == "__main__":
     main()
