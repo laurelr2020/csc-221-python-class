39b6326da56f573700ee675bd1382aa2d2b95f81
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-17 13:21:17 -0400

    created ellispe and oval

--- shape.py
+++ shape.py
@@ -1,3 +1,4 @@
+import math
 class Shape:
     def __init__(self):
         pass
@@ -17,3 +18,14 @@ class Square(Rectangle):
     def __init__(self, sideLength):
         super().__init__(sideLength, sideLength)
 
+class Ellipse(Shape):
+    def __init__(self, width, height):
+        self.width = abs(width)
+        self.height = abs(height)
+
+    def calculateArea(self):
+        return (math.pi * self.width * self.height)
+
+class Oval(Ellipse):
+    def __init__(self, radius):
+        super().__init__(radius, radius)
