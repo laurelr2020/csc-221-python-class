d95028c5cc549e904a0a528cbc30e54f4fcbf180
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-17 12:57:10 -0400

    added rectangle class and init

---
+++ shape.py
@@ -0,0 +1,13 @@
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def calculateArea(self):
+        return 0.0
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = abs(width)
+        self.height = abs(height)
+    
