8fc4cf6bbd8bcfc7650fcee199629654f4908b1e
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-19 16:57:13 -0400

    create main method that instaniates shapes

--- ProgramAssignments/PA2/main.py
+++ ProgramAssignments/PA2/main.py
@@ -2,4 +2,16 @@
 putting them in a list, then using a for loop to polymorphically call each object's calculateArea 
 method and print each result.'''
 
-from shape import Shape
+from shape import Shape, Rectangle, Ellipse, Square, Oval
+
+def main(self):
+    square = Square(5)
+    rectangle = Rectangle(12,5)
+    ellipse = Ellipse(4, 10)
+    oval = Oval(-4)
+
+    shapeList = [square, rectangle, ellipse, oval]
+
+    for s in shapeList:
+        if isinstance(s, Shape):
+            print("This " + s.getShape() + " has an area of " + s.calculateArea())
