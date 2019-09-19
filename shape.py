9b9eb259e84a92b93a8a83263899020ae27f7677
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-19 12:43:36 -0400

    remove extra shape

--- shape.py
+++ shape.py
@@ -18,15 +18,3 @@ class Rectangle(Shape):
 class Square(Rectangle): 
     def __init__(self, sideLength):
         super().__init__(sideLength, sideLength)
-
-class Ellipse(Shape):
-    def __init__(self, width, height):
-        self.width = abs(width)
-        self.height = abs(height)
-
-    def calculateArea(self):
-        return (math.pi * self.width * self.height)
-
-class Oval(Ellipse):
-    def __init__(self, radius):
-        super().__init__(radius, radius)
