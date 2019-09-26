00da9dea260be86180ea9374c6160cb8d1de9d49
laurelr2020 <laurelr2020@mountunion.edu>
2019-09-26 08:24:52 -0400

    read file by line

--- files_and_excpetions/file_reader.py
+++ files_and_excpetions/file_reader.py
@@ -1,7 +1,12 @@
 
-filename = 'text.txt'
+filename = 'read.txt'
 
 #reads in file. prints all contents
+# with open(filename) as file:
+#     contents = file.read()
+# print(contents)
+
+#read in file by line. print each line
 with open(filename) as file:
-    contents = file.read()
-print(contents)
+    for line in file:
+        print(line.rstrip())
